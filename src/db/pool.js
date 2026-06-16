d3d5383c1eef4bccbd0210028224b2feb1d87ced
Leonel <leonelbruno972@gmail.com>
2026-06-16 14:05:35 -0300

    preparé la configuracion para el deployment

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,11 +1,17 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-});
+const poolConfig = process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL
+    }
+    : {
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    };
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const { loadEnvFile } = require("node:process");
-loadEnvFile(".env");
+
+if (process.env.NODE_ENV !== "production") {
+    loadEnvFile(".env");
+}
 
 const app = require("./app");
 const config = require("../config/config");
 
 const PORT = config.PORT;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
